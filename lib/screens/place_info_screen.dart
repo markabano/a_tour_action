1c1263609e45fc1a553bfd7238b2d3bf7770a04a
mabano <markabanomain@gmail.com>
2023-07-31 22:15:40 +0800

    carousel

--- lib/screens/place_info_screen.dart
+++ lib/screens/place_info_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class PlaceInfoScreen extends StatefulWidget {
@@ -17,7 +18,35 @@ class _PlaceInfoScreenState extends State<PlaceInfoScreen> {
         centerTitle: true,
       ),
       body: Column(
-        children: [],
+        children: [
+          const SizedBox(
+            height: 10.0,
+          ),
+          CarouselSlider(
+            items: [
+              1,
+              2,
+              3,
+              4,
+              5,
+            ].map((e) {
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text("sample picture goes here ${e.toString()}"),
+              );
+            }).toList(),
+            options: CarouselOptions(height: 300),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Text("Information goes here"),
+        ],
       ),
     );
   }
