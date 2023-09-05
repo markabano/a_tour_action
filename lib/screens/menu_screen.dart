e8d0d7253998a26c4644307cc3744a33d6bd3b2c
mabano <markabanomain@gmail.com>
2023-09-05 19:57:27 +0800

    menu screen options

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../widgets/bottom_navigation.dart';
@@ -12,16 +14,82 @@ class MenuScreen extends StatefulWidget {
 }
 
 class _MenuScreenState extends State<MenuScreen> {
+  final name = FirebaseAuth.instance.currentUser!.email!.split('@')[0];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            ElevatedButton(
-              onPressed: logout,
-              child: const Text('Logout'),
-            )
+            Container(
+              alignment: Alignment.centerLeft,
+              width: double.infinity,
+              height: 200,
+              color: Colors.lightBlue[100],
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    const CircleAvatar(
+                      radius: 50,
+                      child: Icon(
+                        Icons.person,
+                        size: 100,
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    Text(
+                      name,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Card(
+              child: ListTile(
+                leading: const Icon(Icons.person),
+                title: const Text('Profile'),
+                trailing: const Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+            const Card(
+              child: ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('Settings'),
+                trailing: const Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+            const Card(
+              child: ListTile(
+                leading: const Icon(Icons.info),
+                title: const Text('About'),
+                trailing: const Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+            const Card(
+              child: ListTile(
+                leading: const Icon(Icons.help),
+                title: const Text('Help'),
+                trailing: const Icon(Icons.arrow_forward_ios),
+              ),
+            ),
+            GestureDetector(
+              onTap: logout,
+              child: const Card(
+                child: ListTile(
+                  leading: const Icon(Icons.logout),
+                  title: const Text('Logout'),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                ),
+              ),
+            ),
           ],
         ),
       ),
