736ec79ef18f1a3ae017ec8fdc2eaef9c7dd75a5
rheimark1 <rheimark1@gmail.com>
2023-10-21 20:11:18 +0800

    Category screen update

--- lib/screens/game tab/category_screen.dart
+++ lib/screens/game tab/category_screen.dart
@@ -20,9 +20,10 @@ class _CategoryScreenState extends State<CategoryScreen> {
   }
 
   Future<void> fetchCategoriesFromFirestore() async {
-    final querySnapshot = await FirebaseFirestore.instance.collection('categories').get();
-
-    if (querySnapshot.docs.isNotEmpty) {
+    try {
+  final querySnapshot = await FirebaseFirestore.instance.collection('categories').get();
+  // Rest of the code for processing data
+  if (querySnapshot.docs.isNotEmpty) {
       setState(() {
         categories = querySnapshot.docs.map((doc) {
           return {
@@ -35,6 +36,25 @@ class _CategoryScreenState extends State<CategoryScreen> {
     else {
       const Text('error');
     }
+} catch (error) {
+  print("Error retrieving data from Firestore: $error");
+}
+
+    // final querySnapshot = await FirebaseFirestore.instance.collection('categories').get();
+
+    // if (querySnapshot.docs.isNotEmpty) {
+    //   setState(() {
+    //     categories = querySnapshot.docs.map((doc) {
+    //       return {
+    //         'name': doc['name'],
+    //         'imagePath': doc['imagePath'],
+    //       };
+    //     }).toList();
+    //   });
+    // }
+    // else {
+    //   const Text('error');
+    // }
   }
 
   void handleCategoryTap(int index) {
@@ -53,28 +73,29 @@ class _CategoryScreenState extends State<CategoryScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('hello'),
-            GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Two columns
-              ),
-              itemCount: categories.length, // Number of categories
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    handleCategoryTap(index); // Set the selected category when an image is tapped
-                  },
-                  child: Container(
-                    margin: EdgeInsets.all(8),
-                    child: Image.network(
-                      categories[index]['imagePath'], // Use the image path from Firestore
-                      width: 150, // Adjust the width and height as needed
-                      height: 150,
-                      fit: BoxFit.cover,
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // Two columns
+                ),
+                itemCount: categories.length, // Number of categories
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      handleCategoryTap(index); // Set the selected category when an image is tapped
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      child: Image.network(
+                        categories[index]['imagePath'], // Use the image path from Firestore
+                        width: 150, // Adjust the width and height as needed
+                        height: 150,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
